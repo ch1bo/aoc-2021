2b1a4d46c7d4096e6b1df9df54280c903a770a26
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-12-02 09:00:15 +0100

    Use nom to parse stuff

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,17 +1,55 @@
-use std::fs::File;
 use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
+use nom::character::complete::digit1;
+use nom::sequence::preceded;
+use nom::bytes::complete::tag;
+use nom::Finish;
+
+#[derive(Debug)]
+enum Command {
+    Forward (u64),
+    Down (u64),
+    Up (u64),
+}
+
+fn command(input: &str) -> nom::IResult<&str, Command> {
+    nom::branch::alt((forward, down, up))(input)
+}
+
+// TODO(SN): pass enum constrctors to DRY this?
+fn forward(input: &str) -> nom::IResult<&str, Command> {
+    let (rest, value) = preceded(tag("forward "), digit1)(input)?;
+    Ok((
+      rest,
+      Command::Forward(value.parse().unwrap()) // REVIEW(SN): mapping errors
+    ))
+}
+
+fn down(input: &str) -> nom::IResult<&str, Command> {
+    let (rest, value) = preceded(tag("down "), digit1)(input)?;
+    Ok((
+      rest,
+      Command::Down(value.parse().unwrap()) // REVIEW(SN): mapping errors
+    ))
+}
+
+fn up(input: &str) -> nom::IResult<&str, Command> {
+    let (rest, value) = preceded(tag("up "), digit1)(input)?;
+    Ok((
+      rest,
+      Command::Up(value.parse().unwrap()) // REVIEW(SN): mapping errors
+    ))
+}
 
 fn main() -> io::Result<()> {
-    let file = File::open("input")?;
-    let reader = BufReader::new(file);
+    // let file = File::open("input")?;
+    // let reader = BufReader::new(file);
     // let input: Vec<i64> = reader
     //     .lines()
     //     .map(|l| l.unwrap().parse().unwrap())
     //     .collect();
-    let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    println!("Input: {:?}", input);
+    let input = ["forward 5" , "down 5" , "forward 8" , "up 3" , "down 8" , "forward 2"];
+    let res = input.map(|i| command(i).finish().unwrap().1);
+    println!("Input: {:?}", res);
 
     // Part1
     let part1 = "";
