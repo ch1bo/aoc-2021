2b72374f5b2d969fe91994c0d82d276a52d81fcd
Sebastian Nagel <sebastian.nagel@ncoding.at>
2021-12-03 21:02:38 +0100

    Day2 part1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,17 @@
-use std::io;
+use nom::bytes::complete::tag;
 use nom::character::complete::digit1;
 use nom::sequence::preceded;
-use nom::bytes::complete::tag;
 use nom::Finish;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
 
 #[derive(Debug)]
 enum Command {
-    Forward (u64),
-    Down (u64),
-    Up (u64),
+    Forward(u64),
+    Down(u64),
+    Up(u64),
 }
 
 fn command(input: &str) -> nom::IResult<&str, Command> {
@@ -19,40 +22,58 @@ fn command(input: &str) -> nom::IResult<&str, Command> {
 fn forward(input: &str) -> nom::IResult<&str, Command> {
     let (rest, value) = preceded(tag("forward "), digit1)(input)?;
     Ok((
-      rest,
-      Command::Forward(value.parse().unwrap()) // REVIEW(SN): mapping errors
+        rest,
+        Command::Forward(value.parse().unwrap()), // REVIEW(SN): mapping errors
     ))
 }
 
 fn down(input: &str) -> nom::IResult<&str, Command> {
     let (rest, value) = preceded(tag("down "), digit1)(input)?;
     Ok((
-      rest,
-      Command::Down(value.parse().unwrap()) // REVIEW(SN): mapping errors
+        rest,
+        Command::Down(value.parse().unwrap()), // REVIEW(SN): mapping errors
     ))
 }
 
 fn up(input: &str) -> nom::IResult<&str, Command> {
     let (rest, value) = preceded(tag("up "), digit1)(input)?;
     Ok((
-      rest,
-      Command::Up(value.parse().unwrap()) // REVIEW(SN): mapping errors
+        rest,
+        Command::Up(value.parse().unwrap()), // REVIEW(SN): mapping errors
     ))
 }
 
 fn main() -> io::Result<()> {
-    // let file = File::open("input")?;
-    // let reader = BufReader::new(file);
-    // let input: Vec<i64> = reader
-    //     .lines()
-    //     .map(|l| l.unwrap().parse().unwrap())
-    //     .collect();
-    let input = ["forward 5" , "down 5" , "forward 8" , "up 3" , "down 8" , "forward 2"];
-    let res = input.map(|i| command(i).finish().unwrap().1);
-    println!("Input: {:?}", res);
+    let file = File::open("input")?;
+    let reader = BufReader::new(file);
+    let input: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+    // let input = [
+    //     "forward 5",
+    //     "down 5",
+    //     "forward 8",
+    //     "up 3",
+    //     "down 8",
+    //     "forward 2",
+    // ];
+    let commands = input.iter().map(|i| command(i).finish().unwrap().1);
+    println!("Input: {:?}", commands);
 
     // Part1
-    let part1 = "";
+    let mut pos = (0, 0);
+    for c in commands {
+        match c {
+            Command::Forward(i) => {
+                pos = (pos.0 + i, pos.1);
+            }
+            Command::Down(i) => {
+                pos = (pos.0, pos.1 + i);
+            }
+            Command::Up(i) => {
+                pos = (pos.0, pos.1 - i);
+            }
+        }
+    }
+    let part1 = pos.0 * pos.1;
     println!("Part1: {}", part1);
 
     // Part2
